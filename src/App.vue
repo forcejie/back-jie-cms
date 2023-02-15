88d26df64b06f24981a46f0b5a4ae8db5ccc0d13
forcejie <2963301696@qq.com>
2023-02-15 11:40:06 +0800

    feat: 搭建项目并进行配置

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">
+    <!-- <h2>app</h2>
+    <router-link to="/main">主页</router-link>
+    <router-link to="/login">登录</router-link> -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.app {
+  height: 100vh;
+  width: 100wh;
+  background-color: rgb(250, 228, 231);
+}
+</style>
---
+++ src/views/not-found/not-found.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="not-found">
+    <h2>此页面不存在哦</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.not-found {
+  color: red;
+}
+</style>
